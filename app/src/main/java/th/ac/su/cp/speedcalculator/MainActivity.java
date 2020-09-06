6a3e13e49b9b8a76eec84e8f0d5d59d459b334c1
fangnm <fangnm1940@gmail.com>
2020-09-06 12:25:10 +0700

    midteam

--- app/src/main/java/th/ac/su/cp/speedcalculator/MainActivity.java
+++ app/src/main/java/th/ac/su/cp/speedcalculator/MainActivity.java
@@ -1,6 +1,10 @@
 package th.ac.su.cp.speedcalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button en_cal = (Button) findViewById(R.id.button_cal);
+        final Button en_clear = (Button)findViewById(R.id.button_clear);
+
+        en_cal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView text1 =(EditText)findViewById(R.id.editTextNumberDecimal);
+                TextView text2 =(EditText)findViewById(R.id.editTextNumberDecimal2);
+                TextView text3 =(EditText)findViewById(R.id.textView3);
+
+
+
+
+
+
+
+
+
+
+            }
+        });
     }
 }
